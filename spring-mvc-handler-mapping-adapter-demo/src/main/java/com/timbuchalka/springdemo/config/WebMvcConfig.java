1233059cadd613f596fb6951ac5a6152dfbae055
Aleksey Savin <saleksey67@gmail.com>
2017-08-29 17:40:10 +0300

    worked well, but if option of rmhm.set(false) dont worked

--- spring-mvc-handler-mapping-adapter-demo/src/main/java/com/timbuchalka/springdemo/config/WebMvcConfig.java
+++ spring-mvc-handler-mapping-adapter-demo/src/main/java/com/timbuchalka/springdemo/config/WebMvcConfig.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
@@ -19,11 +21,21 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public DataSource dataSource() {
 		final JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
-			dsLookup.setResourceRef(true);
-			DataSource dataSource = dsLookup.getDataSource("jdbc/springdb");
-			return dataSource;
-		}
-	
+		dsLookup.setResourceRef(true);
+		DataSource dataSource = dsLookup.getDataSource("jdbc/springdb");
+		return dataSource;
+	}
+
+	@Bean
+	public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+		RequestMappingHandlerMapping rmhm = new RequestMappingHandlerMapping();
+		rmhm.setUseSuffixPatternMatch(false);
+		rmhm.setUseTrailingSlashMatch(false);
+		
+		return rmhm;
+
+	}
+
 	@Bean
 	public UrlBasedViewResolver urlBasedViewResolver() {
 		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
@@ -33,4 +45,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		return resolver;
 	}
 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+//		<mvc:view-controller path="/" view-name="home" />
+	}
+
+	
 }
