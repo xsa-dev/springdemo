061034e0f5c647a73b0b0bfb561351a5bca877fb
Aleksey Savin <saleksey67@gmail.com>
2017-09-29 17:25:13 +0300

    id dont sure ready this for 108 video or not

--- spring-mvc-request-mapping-param-demo/src/main/java/com/timbuchalka/springdemo/controller/test/RequestMappingAndParamDemoController.java
+++ spring-mvc-request-mapping-param-demo/src/main/java/com/timbuchalka/springdemo/controller/test/RequestMappingAndParamDemoController.java
@@ -3,10 +3,25 @@ package com.timbuchalka.springdemo.controller.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping(value="/requestMapppingAndParamDemo")
 @Controller
 public class RequestMappingAndParamDemoController {
 	private static Logger LOGGER = LoggerFactory.getLogger(RequestMappingAndParamDemoController.class);
+	
+	@RequestMapping(value="/home")
+	public String home() {
+		return "requesMappingAndParamHome";
+	}
+	
+	//test 1: Testing @RequestParam without explicit attributes
+	@RequestMapping
+	public String requestMappingAndParamTest1(@RequestParam("orgName") String orgName, Model model) {
+		model.addAttribute("orgname", orgName);
+		model.addAttribute("testSerial", "test1");
+		return "requestMappingAndParamResults";
+	}
 }
