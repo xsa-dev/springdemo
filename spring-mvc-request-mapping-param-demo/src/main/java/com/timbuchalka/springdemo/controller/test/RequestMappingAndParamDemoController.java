1c15079081c970340fc8f38c346c1effd66bd90b
Aleksey Savin <saleksey67@gmail.com>
2017-10-16 17:44:57 +0300

    video 109 done

--- spring-mvc-request-mapping-param-demo/src/main/java/com/timbuchalka/springdemo/controller/test/RequestMappingAndParamDemoController.java
+++ spring-mvc-request-mapping-param-demo/src/main/java/com/timbuchalka/springdemo/controller/test/RequestMappingAndParamDemoController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping(value="/requestMappingAndParamDemo")
@@ -24,5 +25,20 @@ public class RequestMappingAndParamDemoController {
 		model.addAttribute("orgname", orgName);
 		model.addAttribute("testserial", "test1");
 		return "requestMappingAndParamResults";
+	}
+	
+	// test 2: Testing @RequestMapping ''method'' attribute
+	@RequestMapping(value="test2", method = RequestMethod.GET)
+	public String requestMappingAndParamTest2(@RequestParam("orgname") String orgName, Model model) {
+		model.addAttribute("orgname", orgName);
+		model.addAttribute("testserial", "test1");
+		return "requestMappingAndParamResults";
+	}
+	// test 3: Testing @RequestMapping fall back feature
+	@RequestMapping(value="*", method = RequestMethod.GET)
+//	@RequestMapping(value="*", method = {RequestMethod.GET, RequestMethod.POST})
+
+	public String requestMappingAndParamTest3() {
+		return "fallback";
 	}
 }
