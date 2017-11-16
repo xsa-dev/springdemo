44467d2350f52cbf8c176014e994647e4ce3e9a6
Aleksey Savin <saleksey67@gmail.com>
2017-11-16 08:58:19 +0300

    video 121 added. But Nothing Tested

---
+++ spring-mvc-session-request-attribute-demo/src/main/java/com/timbuchalka/springdemo/domain/test/VisitorData.java
@@ -0,0 +1,49 @@
+package com.timbuchalka.springdemo.domain.test;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisitorData implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5075218314653537512L;
+
+	private String currentVisitorName;
+	private String currentVisitorEmail;
+	private List<Visitor> visitor = new ArrayList<Visitor>();
+	public VisitorData(String currentVisitorName, String currentVisitorEmail, List<Visitor> visitors) {
+		super();
+		this.currentVisitorName = currentVisitorName;
+		this.currentVisitorEmail = currentVisitorEmail;
+		this.visitor = visitors;
+	}
+	public String getCurrentVisitorName() {
+		return currentVisitorName;
+	}
+	public void setCurrentVisitorName(String currentVisitorName) {
+		this.currentVisitorName = currentVisitorName;
+	}
+	public String getCurrentVisitorEmail() {
+		return currentVisitorEmail;
+	}
+	public void setCurrentVisitoEmail(String currentVisitorEmail) {
+		this.currentVisitorEmail = currentVisitorEmail;
+	}
+	
+	
+	
+	public List<Visitor> getVisitor() {
+		return visitor;
+	}
+	public void setVisitor(List<Visitor> visitor) {
+		this.visitor = visitor;
+	}
+	@Override
+	public String toString() {
+		return "VisitorData [currentVisitorName=" + currentVisitorName + ", currentVisitorEmail=" + currentVisitorEmail
+				+ ", visitor=" + visitor + "]";
+	}
+}
