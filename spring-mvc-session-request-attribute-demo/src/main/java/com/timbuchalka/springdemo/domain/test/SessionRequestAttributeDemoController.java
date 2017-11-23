1c2c3b59cb3dd6b95735033b4c88955fd831058b
Aleksey Savin <saleksey67@gmail.com>
2017-11-23 14:36:42 +0300

    updated. ready for video 124

--- spring-mvc-session-request-attribute-demo/src/main/java/com/timbuchalka/springdemo/domain/test/SessionRequestAttributeDemoController.java
+++ spring-mvc-session-request-attribute-demo/src/main/java/com/timbuchalka/springdemo/domain/test/SessionRequestAttributeDemoController.java
@@ -28,7 +28,7 @@ public class SessionRequestAttributeDemoController {
 	private static Logger LOGGER = LoggerFactory.getLogger(SessionRequestAttributeDemoController.class);
 	
 	public ModelAndView home() {
-		return new ModelAndView("sessionRequestAttributeHome", "visitorStats", new VisitorData());
+		return new ModelAndView("test/sessionRequestAttributeViews/sessionRequestAttributeHome", "visitorStats", new VisitorData());
 	}
 	
 	@ModelAttribute("visitordata")
@@ -66,7 +66,7 @@ public class SessionRequestAttributeDemoController {
 		
 		}
 		
-		return "sessionRequestAttributeResult";
+		return "test/sessionRequestAttributeViews/sessionRequestAttributeResult";
 		
 	}
 }
